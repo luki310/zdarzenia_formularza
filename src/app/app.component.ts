829e680cf263429b8c9ec132ade31ea428d2ab1b
Łukasz Kuczek <lkuczek@wp.pl>
2017-11-13 13:27:34 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Usuwanie playlist i bąbelkowanie zdarzeń';
+
+  constructor() {}
+}
